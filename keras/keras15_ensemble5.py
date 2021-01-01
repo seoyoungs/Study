99d668b6d61cc468177eb670ba1470501fae161d
seoyoung <ka005740@gmail.com>
2021-01-02 00:27:11 +0900

    0102

--- keras/keras15_ensemble5.py
+++ keras/keras15_ensemble5.py
@@ -1,4 +1,4 @@
-#1:다 앙상블
+####metrics가 다른 경우-->3. 컴파일 에서
 
 import numpy as np
 
@@ -7,24 +7,20 @@ import numpy as np
 x1=np.array([range(100),range(301, 401), range(1,101)])
 y1=np.array([range(711,811), range(1,101), range(201,301)])#각100개
 
-#x2=np.array([range(101, 201), range(411,511), range(100, 200)])
+x2=np.array([range(101, 201), range(411,511), range(100, 200)])
 y2=np.array([range(501, 601), range(711, 811), range(100)])
 #3행 100열 모델
 
 x1= np.transpose(x1)
-#x2= np.transpose(x2)
+x2= np.transpose(x2)
 y1= np.transpose(y1)
 y2= np.transpose(y2)
-
-#pred
-x_pred2=np.array([100,302,101]) #행렬 바꾸기1
-x_pred2=x_pred2.reshape(1,3)
 
 from sklearn.model_selection import train_test_split
 x1_train, x1_test, y1_train, y1_test =train_test_split(
     x1, y1, shuffle=False, train_size=0.8) #shuffle=False순서대로
-y2_train, y2_test =train_test_split(y2, 
-                                shuffle=False, train_size=0.8)
+x2_train, x2_test, y2_train, y2_test =train_test_split(
+    x2, y2, shuffle=False, train_size=0.8)
 
 #모델링 할 때 Sequential로 하면 순서대로 한다는 뜻이다
 #근데 이 두모델은 수평적 관계라 순차적인 순위를 매기기 힘듬
@@ -41,14 +37,6 @@ dense1=Dense(10, activation='relu')(input1)
 dense1=Dense(5, activation='relu')(dense1)
 #output1=Dense(3)(dense1)
 
-model = Sequential()
-model.add(Dense(6, input_dim=1, activation='linear'))
-model.add(Dense(5, activation='linear'))
-model.add(Dense(8))
-model.add(Dense(1))
-model.summary()
-
-'''
 #모델 2
 input2= Input(shape=(3,))
 dense2=Dense(10, activation='relu')(input2)
@@ -65,49 +53,56 @@ merge1 = concatenate([dense1,dense2]) #모델 1,2 병합
 middle1=Dense(30)(merge1) #모델 합친후에도 layer추가 가능
 middle1=Dense(10)(middle1)
 middle1=Dense(10)(middle1) 
-'''
+
+
 #데이터 합친 것 다시 나누기(100,3)2개니까 (100,6)된것 다시 나누기
 #모델 분기1
-output1= Dense(30)(dense1)
+output1= Dense(30)(middle1)
 output1=Dense(7)(output1)
 output1=Dense(3)(output1) #여기까지가 y1
 
 #모델 분기2
-output2= Dense(30)(dense1)
+output2= Dense(30)(middle1)
 output2=Dense(7)(output2)
 output2=Dense(7)(output2)
 output2=Dense(3)(output2) #여기까지가 y2
 
 #모델 선언 (함수형으로)
-model = Model(inputs=[input1], 
+model = Model(inputs=[input1,input2], 
               outputs=[output1, output2]) #두개 이상은 list로 묶는다.
 model.summary()
 
 #3. 컴파일, 훈련
-model.compile(loss='mse', optimizer='adam', metrics=['mse'])
-model.fit([x1_train], [y1_train, y2_train],
-           epochs=10, batch_size=1, validation_split=0.2, verbose=1) 
+model.compile(loss='mse', optimizer='adam', metrics=['mae','mse'])
+dd=model.fit([x1_train, x2_train], [y1_train, y2_train],
+           epochs=10, batch_size=1, validation_split=0.2, verbose=0) 
 #모델 2개이므로 원래 x_train, y_train 넣었는데 2개이므로 list사용
 #모델 2개 이므로 list([])로 묶는다.
 
 #4. 평가, 예측
-loss= model.evaluate([x1_test], [y1_test, y2_test]
+loss= model.evaluate([x1_test, x2_test], [y1_test, y2_test]
                       ,batch_size=1)
 print(loss)
+
 '''
 #concatenate 뜻
 metrics=['mse']로 했을 때
 [2737.472412109375, 1091.6365966796875, 1645.8359375, 1091.6365966796875, 1645.8359375] 
+첫로스+두번째 로스의 산술합 / 첫 모델의 로스 /두번째 모델의 로스 / 첫번째 메트릭스 / 두번째 메트릭스
 [1091.6365966796875, 1645.8359375]을 합한 값은 2384.06982421875
 loss의 mse와 metrics의 mae값이 같다
 concatenate값1개, loss값 2개, metrics값2개 해서 총 5개 값이 나온다.
+
 '''
+
+
 print("model.metrics_names :", model.metrics_names)
 # model.metrics_names : ['loss', 'dense_11_loss', 'dense_15_loss', 'dense_11_mse', 'dense_15_mse']
 #이걸보면 print(loss) 했을 때 값 알 수 있다. summary 참조
 
-#이 y_predict는 RMSE를 구하기 위한 지표이구 실제 y_pred가 아니다.
-y1_predict, y2_predict =model.predict(x1_test)
+y1_predict, y2_predict =model.predict([x1_test, x2_test]) 
+
+
 '''
 print("==============")
 print("y1_predict : ",y1_predict)
@@ -116,6 +111,7 @@ print("y2_predict :", y2_predict)
 print("==============")
 #test_size가 20개 이므로 20개나온다.
 '''
+
 #사이킷런 RMSE
 from sklearn.metrics import mean_squared_error
 # def 뜻 RMSE라는 함수 정의 하겠다는 뜻 
@@ -140,7 +136,3 @@ R2=(R2_1+R2_2)/2
 print('R2_1 : ', R2_1)
 print('R2_2 : ', R2_2)
 print('R2 : ', R2)
-
-#다만들면 predict의 일부값을 출력하시오
-y_pred=model.predict(x_pred2)
-print(y_pred)
---
+++ keras/keras18_boston2_minmax.py
@@ -0,0 +1,100 @@
+##머신러닝 데이터는 sklearn에서 많이 제공한다.
+#########데이터 전처리를 통해 0~1까지로 만들기
+#######데이터 받으면 전처리는 원래 반드시 해야한다.
+
+import numpy as np #gpu단순연산에 좋다.
+
+from sklearn.datasets import load_boston
+#사이킷런에서 데이터 기본 제공
+
+#1. 데이터
+dataset= load_boston()
+x=dataset.data
+y=dataset.target
+#print(x.shape) #(506, 13)
+#print(y.shape)  #(506,)
+#print("==============")
+#print(x[:5])
+#print(y[:10])
+
+'''
+전처리 방법 1(0~1사이로 전처리)
+print(x[:5])
+[6.3200e-03 1.8000e+01 2.3100e+00 0.0000e+00 5.3800e-01 6.5750e+00
+  6.5200e+01 4.0900e+00 1.0000e+00 2.9600e+02 1.5300e+01 3.9690e+02
+  4.9800e+00] ----> 이런게 5개씩 13열---> 이렇게 데이터 전처리를 해야한다.
+ -- 0~1사이에 있다.
+print(y[:10])
+[24.  21.6 34.7 33.4 36.2 28.7 22.9 27.1 16.5 18.9]---> 값 다양(회귀모델)
+'''
+
+print(np.max(x), np.min(x)) #711.0 0.0(이렇게 1보다 클수 있다. 전처리방법 다양) 
+#이걸보면 전처리 전 데이터인 것을 알 수 있다.(0~1사이가 아니기 때문)
+#print(dataset.feature_names) #x의 데이터 이름들
+#print(dataset.DESCR) #데이터 셋 이름과 설명
+
+#####데이터 전처리 (minMax)
+x = x/711. #왜 711. 하는 이유---타입 때문에(실수형)
+#그냥 쓰면 711 정수형이 된다.
+# X=(X - 최소) / (최대 - 최소)
+#  =(x - np.min(x)) / (np.max(x) - np.min(x))
+
+#데이터 처리 (train, test분리 --필수!!!)
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test =train_test_split(x, y, shuffle=True, 
+                                           train_size=0.8, random_state=66) #랜덤지정
+
+#2. 모델 구성
+from tensorflow.keras.models import Sequential, Model 
+from tensorflow.keras.layers import Dense, Input
+
+model=Sequential()
+model.add(Dense(12, activation='relu', input_dim=13)) #이거 하나만 하면 훈련값 제대로 안나온다.
+model.add(Dense(4, activation='relu'))
+model.add(Dense(11))
+model.add(Dense(8))
+model.add(Dense(9))
+model.add(Dense(5))
+model.add(Dense(3))
+model.add(Dense(1))
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+model.fit(x_train, y_train, epochs=100, batch_size=1,
+          validation_split=0.2, verbose=0)
+
+#4. 평가예측
+loss, mae=model.evaluate(x_test, y_test,batch_size=1)
+print('loss:', loss)
+print('mae :', mae)
+#print('loss, mae :', loss, mae)
+y_predict=model.predict(x_test) 
+
+#RMSE
+from sklearn.metrics import mean_squared_error
+def RMSE(y_test, y_predict):
+    return np.sqrt(mean_squared_error(y_test, y_predict))
+print("RMSE : ", RMSE(y_test,y_predict)) 
+
+#R2 (accuracy대산 R2를 사용한다.)
+from sklearn.metrics import r2_score
+r2 = r2_score(y_test, y_predict)
+print("R2 : ", r2)
+
+'''
+전처리 전
+결과
+loss: 19.869380950927734
+mae : 3.2330851554870605
+RMSE :  4.457507891337804
+R2 :  0.7622795125940092
+
+전처리 후
+loss: 11.747001647949219
+mae : 2.6047136783599854
+RMSE :  3.427389877968649
+R2 :  0.859456944795199
+
+이렇게 전처리 하면 성능이 아주조금 향상된다.
+반드시 전처리 해야한다.
+'''
